c9cd0344f74d78a3cff7a856b58d23d05c1c0335
Matt Huszagh <huszaghmatt@gmail.com>
2019-10-11 18:06:12 -0700

    Update shell nix file

--- shell.nix
+++ shell.nix
@@ -13,9 +13,39 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     mh-python
     bitmanip
+
+    # fpga
+    yosys
+    symbiyosys
+    verilator
+    verilog
+    nextpnr
+    gtkwave
+    python3Packages.xdot # TODO is this necessary?
+    graphviz # TODO should this be a yosys dep?
+    vivado-2017-2
+
+    # ftdi
+    openocd
+    libftdi1
+
+    # pcb cad
+    kicad
+
+    # ems
+    openems
+    appcsxcad
+    hyp2mat
+
+    # 3d printing
+    openscad
+    (cura.override { plugins = [ pkgs.curaPlugins.octoprint ]; })
+
+    # video
+    ffmpeg
   ];
 
-  shellHook = ''
-    export PYTHONPATH="$(python -c "import site; print(site.getsitepackages()[0])")"
-  '';
+  # shellHook = ''
+  #   export PYTHONPATH="$(python -c "import site; print(site.getsitepackages()[0])")"
+  # '';
 }
