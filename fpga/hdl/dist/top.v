39b6104782e0f9f6ab73c48db0f4203f25e1a19e
Matt Huszagh <huszaghmatt@gmail.com>
2019-10-16 12:48:25 -0700

    Use new FT245 module for sending data to host PC
    This has a number of advantages over the previous
    implementation. Because it uses an asynchronous
    FIFO internally, CDC issues are no longer a
    concern. This also means you can change the source
    of transmission data (e.g. betwen FIR, FFT, etc.)
    without changing the logic. All you need to change
    is the write clock.

--- fpga/hdl/dist/top.v
+++ fpga/hdl/dist/top.v
@@ -25,7 +25,7 @@ module top #(
    input wire                             ft_rxf_n_i, /* Low when there is data in the buffer that can be read. */
    input wire                             ft_txe_n_i, /* Low when there is room for transmission data in the FIFO. */
    output wire                            ft_rd_n_o, /* Drive low to load read data to ft_data_io each clock cycle. */
-   output reg                             ft_wr_n_o, /* Drive low to write ft_data_io to FIFO for transmission. */
+   output wire                            ft_wr_n_o, /* Drive low to write ft_data_io to FIFO for transmission. */
    output wire                            ft_siwua_n_o, /* Flush transmission data to USB immediately. */
    input wire                             ft_clkout_i, /* 60MHz clock used to synchronize data transfers. */
    output wire                            ft_oe_n_o, /* Drive low one period before ft_rd_n_o to signal read. */
@@ -150,49 +150,45 @@ module top #(
    end
 
    wire                            adf_config_done;
-
    adf4158 adf4158 (
-      .clk         (clk_i),
-      .clk_20mhz   (clk_20mhz),
-      .rst_n       (pll_lock),
-      .enable      (pll_lock),
-      .config_done (adf_config_done),
-
-      .ce          (adf_ce_o),
-      .muxout      (adf_muxout_i),
-      .txdata      (adf_txdata_o),
-      .data        (adf_data_o)
+      .clk         (clk_i           ),
+      .clk_20mhz   (clk_20mhz       ),
+      .rst_n       (pll_lock        ),
+      .enable      (pll_lock        ),
+      .config_done (adf_config_done ),
+      .ce          (adf_ce_o        ),
+      .muxout      (adf_muxout_i    ),
+      .txdata      (adf_txdata_o    ),
+      .data        (adf_data_o      )
    );
 
    wire signed [ADC_DATA_WIDTH-1:0] chan_a;
    wire signed [ADC_DATA_WIDTH-1:0] chan_b;
-
    ltc2292 ltc2292 (
-      .clk (clk_i),
-      .di  (adc_d_i),
-      .dao (chan_a),
-      .dbo (chan_b)
+      .clk (clk_i   ),
+      .di  (adc_d_i ),
+      .dao (chan_a  ),
+      .dbo (chan_b  )
    );
 
    wire signed [FIR_OUTPUT_WIDTH-1:0] chan_a_filtered;
    wire                               fir_dvalid;
-
    fir_poly #(
-      .N_TAPS         (120),
-      .M              (20),
-      .BANK_LEN       (6),
-      .INPUT_WIDTH    (12),
-      .TAP_WIDTH      (16),
-      .INTERNAL_WIDTH (35),
-      .NORM_SHIFT     (4),
-      .OUTPUT_WIDTH   (14)
+      .N_TAPS         (120 ),
+      .M              (20  ),
+      .BANK_LEN       (6   ),
+      .INPUT_WIDTH    (12  ),
+      .TAP_WIDTH      (16  ),
+      .INTERNAL_WIDTH (35  ),
+      .NORM_SHIFT     (4   ),
+      .OUTPUT_WIDTH   (14  )
    ) fir (
-      .clk             (clk_i),
-      .rst_n           (adf_config_done),
-      .clk_2mhz_pos_en (clk_2mhz_pos_en),
-      .din             (chan_a),
-      .dout            (chan_a_filtered),
-      .dvalid          (fir_dvalid)
+      .clk             (clk_i           ),
+      .rst_n           (adf_config_done ),
+      .clk_2mhz_pos_en (clk_2mhz_pos_en ),
+      .din             (chan_a          ),
+      .dout            (chan_a_filtered ),
+      .dvalid          (fir_dvalid      )
    );
 
    reg [N_WIDTH-1:0]                  fir_ctr;
@@ -208,19 +204,19 @@ module top #(
 
    wire signed [FIR_OUTPUT_WIDTH-1:0] fft_in;
    ram_tdp_18k #(
-      .ADDRESS_WIDTH (N_WIDTH),
-      .DATA_WIDTH    (FIR_OUTPUT_WIDTH)
+      .ADDRESS_WIDTH (N_WIDTH          ),
+      .DATA_WIDTH    (FIR_OUTPUT_WIDTH )
    ) ram (
-      .clk1  (clk_i),
-      .clk2  (clk_7_5mhz),
-      .en1   (clk_2mhz_pos_en),
-      .en2   (fft_en),
-      .we1   (1'b1),
-      .we2   (1'b0),
-      .addr1 (fir_ctr),
-      .addr2 (fft_bit_normal_ctr),
-      .di1   (chan_a_filtered),
-      .do2   (fft_in)
+      .clk1  (clk_i              ),
+      .clk2  (clk_i              ),
+      .en1   (clk_2mhz_pos_en    ),
+      .en2   (fft_en             ),
+      .we1   (1'b1               ),
+      .we2   (1'b0               ),
+      .addr1 (fir_ctr            ),
+      .addr2 (fft_bit_normal_ctr ),
+      .di1   (chan_a_filtered    ),
+      .do2   (fft_in             )
    );
 
    reg               fft_en;
@@ -233,104 +229,78 @@ module top #(
       end
    end
 
-   (* ASYNC_REG = "TRUE" *) reg [N_WIDTH-1:0] fir_ctr_cdc;
-   (* ASYNC_REG = "TRUE" *) reg [N_WIDTH-1:0] fir_ctr_stbl;
-   always @(posedge clk_7_5mhz) begin
+   always @(posedge clk_i) begin
       if (!rst_n) begin
-         fft_en       <= 1'b0;
-         fir_ctr_cdc  <= {N_WIDTH{1'b0}};
-         fir_ctr_stbl <= {N_WIDTH{1'b0}};
+         fft_en <= 1'b0;
       end else begin
-         {fir_ctr_stbl, fir_ctr_cdc} <= {fir_ctr_cdc, fir_ctr};
          if (fft_ctr == FFT_N-1) begin
-            fft_en  <= 1'b0;
-         end else if (fir_ctr_stbl == FFT_N-3 || fft_en) begin
-            fft_en  <= 1'b1;
+            fft_en <= 1'b0;
+         end else if (fir_ctr == FFT_N-1 || fft_en) begin
+            fft_en <= 1'b1;
          end
       end
    end
 
    localparam FFT_OUTPUT_WIDTH = 25;
-
    wire fft_valid;
    wire [N_WIDTH-1:0] fft_ctr;
    wire signed [FFT_OUTPUT_WIDTH-1:0] fft_re_o;
    wire signed [FFT_OUTPUT_WIDTH-1:0] fft_im_o;
-
    fft_r22sdf #(
-      .N             (1024),
-      .INPUT_WIDTH   (14),
-      .TWIDDLE_WIDTH (10),
-      .OUTPUT_WIDTH  (FFT_OUTPUT_WIDTH)
+      .N             (1024             ),
+      .INPUT_WIDTH   (14               ),
+      .TWIDDLE_WIDTH (10               ),
+      .OUTPUT_WIDTH  (FFT_OUTPUT_WIDTH )
    ) fft (
-      .clk_i      (clk_7_5mhz),
-      .clk_3x_i   (clk_22_5mhz),
-      .rst_n      (fft_en),
-      .sync_o     (fft_valid),
-      .data_ctr_o (fft_ctr),
-      .data_re_i  (fft_in),
-      .data_im_i  ({FIR_OUTPUT_WIDTH{1'b0}}),
-      .data_re_o  (fft_re_o),
-      .data_im_o  (fft_im_o)
+      .clk_i      (clk_i                    ),
+      .clk_3x_i   (clk_120mhz               ),
+      .rst_n      (fft_en                   ),
+      .sync_o     (fft_valid                ),
+      .data_ctr_o (fft_ctr                  ),
+      .data_re_i  (fft_in                   ),
+      .data_im_i  ({FIR_OUTPUT_WIDTH{1'b0}} ),
+      .data_re_o  (fft_re_o                 ),
+      .data_im_o  (fft_im_o                 )
    );
 
-   // usb
-   // Data is transmitted to the host PC in packets of 8 bytes.
-   reg                         start;
-   always @(posedge clk_7_5mhz) begin
-      if (!rst_n) begin
-         start <= 1'b0;
-      end else begin
-         start <= 1'b1;
-      end
-   end
-
-   reg [2:0]                   tx_byte_ctr;
-   reg signed [USB_DATA_WIDTH-1:0] ft_data;
-   assign ft_data_io = !ft_wr_n_o ? ft_data : 1'bz;
-
-
-   (* ASYNC_REG = "TRUE" *) reg [N_WIDTH-1:0]           fft_ctr_cdc;
-   (* ASYNC_REG = "TRUE" *) reg signed [FFT_OUTPUT_WIDTH-1:0] fft_re_cdc;
-   (* ASYNC_REG = "TRUE" *) reg signed [FFT_OUTPUT_WIDTH-1:0] fft_im_cdc;
-
-   always @(negedge ft_clkout_i) begin
-      if (!rst_n) begin
-         tx_byte_ctr <= 3'd0;
-         ft_wr_n_o   <= 1'b1;
-      end else begin
-         ft_wr_n_o <= 1'b0;
-         if (start) begin
-            tx_byte_ctr <= tx_byte_ctr + 1'b1;
-         end
-
-         fft_ctr_cdc <= fft_ctr;
-         fft_re_cdc <= fft_re_o;
-         fft_im_cdc <= fft_im_o;
-
-         if (fft_valid) begin
-            case (tx_byte_ctr)
-            3'd0: ft_data <= {4'hf, fft_ctr_cdc[9:6]};
-            3'd1: ft_data <= {fft_ctr_cdc[5:0], fft_re_cdc[24:23]};
-            3'd2: ft_data <= fft_re_cdc[22:15];
-            3'd3: ft_data <= fft_re_cdc[14:7];
-            3'd4: ft_data <= {fft_re_cdc[6:0], fft_im_cdc[24:24]};
-            3'd5: ft_data <= fft_im_cdc[23:16];
-            3'd6: ft_data <= fft_im_cdc[15:8];
-            3'd7: ft_data <= fft_im_cdc[7:0];
-            endcase
-         end else begin
-            ft_data <= {USB_DATA_WIDTH{1'b0}};
-         end
-      end
-   end
+   localparam FT245_DATA_WIDTH = 64;
+   wire                               ft245_wrfifo_full;
+   wire                               ft245_rdfifo_full;
+   wire                               ft245_rdfifo_empty;
+   wire signed [FT245_DATA_WIDTH-1:0] ft245_rddata;
+   ft245 #(
+      .WRITE_DEPTH  (1024             ),
+      .READ_DEPTH   (512              ),
+      .DATA_WIDTH   (FT245_DATA_WIDTH ),
+      .DUPLICATE_TX (1                )
+   ) ft245 (
+      .rst_n        (rst_n                                 ),
+      .clk          (clk_i                                 ),
+      .wren         (!ft245_wrfifo_full                    ),
+      .wrdata       ({1'b1, {43{1'b0}}, chan_a, {8{1'b0}}} ), // TODO temp
+      .wrfifo_full  (ft245_wrfifo_full                     ),
+      .rden         (!ft245_rdfifo_empty                   ),
+      .rddata       (ft245_rddata                          ),
+      .rdfifo_full  (ft245_rdfifo_full                     ),
+      .rdfifo_empty (ft245_rdfifo_empty                    ),
+      .ft_clk       (ft_clkout_i                           ),
+      .slow_ft_clk  (clk_7_5mhz                            ),
+      .rxf_n        (ft_rxf_n_i                            ),
+      .txe_n        (ft_txe_n_i                            ),
+      .rd_n         (ft_rd_n_o                             ),
+      .wr_n         (ft_wr_n_o                             ),
+      .oe_n         (ft_oe_n_o                             ),
+      .suspend_n    (ft_suspend_n_i                        ),
+      .ft_siwua_n   (ft_siwua_n_o                          ),
+      .ft_data      (ft_data_io                            )
+   );
 
 endmodule
 
 `ifdef TOP_SIMULATE
 
-`include "MMCME2_BASE.v"
-`include "MMCME2_ADV.v"
+`include "PLLE2_BASE.v"
+`include "PLLE2_ADV.v"
 `include "BRAM_SINGLE_MACRO.v"
 `include "BRAM_SDP_MACRO.v"
 `include "BRAM_TDP_MACRO.v"
@@ -395,40 +365,40 @@ module top_tb;
    wire [5:0] ext1;
 
    top dut (
-      .clk_i          (clk_40mhz),
-      .led_o          (led),
-      .ext1_io        (ext1),
+      .clk_i          (clk_40mhz  ),
+      .led_o          (led        ),
+      .ext1_io        (ext1       ),
 
-      .ft_data_io     (ft_data_io),
+      .ft_data_io     (ft_data_io ),
       // .ft_rxf_n_i,
-      // .ft_txe_n_i,
-      .ft_rd_n_o      (ft_rd_n),
-      .ft_wr_n_o      (ft_wr_n),
-      .ft_siwua_n_o   (ft_siwua_n),
-      .ft_clkout_i    (clk_60mhz),
-      .ft_oe_n_o      (ft_oe_n),
-      .ft_suspend_n_i (1'b1),
+      .ft_txe_n_i     (1'b0       ),
+      .ft_rd_n_o      (ft_rd_n    ),
+      .ft_wr_n_o      (ft_wr_n    ),
+      .ft_siwua_n_o   (ft_siwua_n ),
+      .ft_clkout_i    (clk_60mhz  ),
+      .ft_oe_n_o      (ft_oe_n    ),
+      .ft_suspend_n_i (1'b1       ),
 
-      .adc_d_i        (sample_in),
+      .adc_d_i        (sample_in  ),
       // .adc_of_i,
-      .adc_oe_o       (adc_oe),
-      .adc_shdn_o     (adc_shdn),
+      .adc_oe_o       (adc_oe     ),
+      .adc_shdn_o     (adc_shdn   ),
 
       // .sd_data_i,
       // .sd_cmd_i,
       // .sd_clk_o,
       // .sd_detect_i,
 
-      .mix_enbl_n_o   (mix_enbl_n),
+      .mix_enbl_n_o   (mix_enbl_n ),
 
-      .pa_en_n_o      (pa_en_n),
+      .pa_en_n_o      (pa_en_n    ),
 
-      .adf_ce_o       (adf_ce),
-      .adf_le_o       (adf_le),
-      .adf_clk_o      (adf_clk),
+      .adf_ce_o       (adf_ce     ),
+      .adf_le_o       (adf_le     ),
+      .adf_clk_o      (adf_clk    ),
       // .adf_muxout_i,
-      .adf_txdata_o   (adf_txdata),
-      .adf_data_o     (adf_data)
+      .adf_txdata_o   (adf_txdata ),
+      .adf_data_o     (adf_data   )
       // .adf_done_i
 
       // .flash_cs_n_o,
