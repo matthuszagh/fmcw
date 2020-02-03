4783d4e2d6c0957be177dbe4eb3e8b5511632e42
Matt Huszagh <huszaghmatt@gmail.com>
2020-02-03 15:06:14 -0800

    move main pcb hardware to boards+version subdirectory

--- hardware/antenna/patch_1x1x5/generate_model.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pcb_file=patch_1x1x5.kicad_pcb
-config_file=config.json
-
-# Generate octave script files with model and mesh grid
-pcbmodelgen -p $pcb_file -c $config_file -m kicad_pcb_model.m -g kicad_pcb_mesh.m
