e13b779f4fc16485fd578ecbaac82a0a3565dd59
Matt Huszagh <huszaghmatt@gmail.com>
2020-02-23 06:14:46 -0800

    add pll sync ctr formal verification

--- gateware/pll_sync_ctr.v
+++ gateware/pll_sync_ctr.v
@@ -6,8 +6,13 @@
 
 `include "dual_ff.v"
 
+// Generates a counter reflecting the phase relationship between 2
+// synchronous clocks with different frequencies. Specifically, when
+// the positive clock edge of the slow clock triggers, the counter
+// will be set to 0 and will increment every fast clock period until
+// the next slow clock edge.
 module pll_sync_ctr #(
-   /* ratio of slow clock period to fast clock period */
+   // ratio of slow clock period to fast clock period
    parameter RATIO = 8
 ) (
    input wire                     fst_clk,
@@ -33,25 +38,84 @@ module pll_sync_ctr #(
    );
 
    always @(posedge fst_clk) begin
-      last_slw_clk <= slw_clk_tmp;
-      if (!last_slw_clk && slw_clk_tmp) begin
-         ctr <= {{$clog2(RATIO)-1{1'b0}}, 1'b1};
+      if (!rst_n) begin
+         ctr <= {$clog2(RATIO){1'b0}};
       end else begin
-         if (ctr == RATIO_CMP-1'b1)
-           ctr <= {$clog2(RATIO){1'b0}};
-         else
-           ctr <= ctr + 1'b1;
+         last_slw_clk <= slw_clk_tmp;
+         if (!last_slw_clk && slw_clk_tmp) begin
+            ctr <= {{$clog2(RATIO)-1{1'b0}}, 1'b1};
+         end else begin
+            if (ctr == RATIO_CMP-1'b1)
+              ctr <= {$clog2(RATIO){1'b0}};
+            else
+              ctr <= ctr + 1'b1;
+         end
       end
    end
 
+`ifdef FORMAL
+ `ifdef PLL_SYNC_CTR
+  `define ASSUME assume
+ `else
+  `define ASSUME assert
+ `endif
+
+   (* gclk *) reg gclk;
+   integer gctr                       = 0;
+   reg [$clog2(RATIO)-1:0] out_ctr    = 0;
+   reg                     past_valid = 0;
+   always @(posedge gclk) begin
+      gctr <= gctr + 1;
+      past_valid <= 1;
+      if ($initstate) begin
+         `ASSUME (fst_clk == 1'b0);
+         `ASSUME (slw_clk == 1'b0);
+      end else begin
+         `ASSUME ($changed(fst_clk));
+         if (gctr == 0) begin
+            `ASSUME (slw_clk == 1);
+         end
+      end
+
+      if (past_valid) begin
+         if ($past(out_ctr) == 7 && $past(rst_n) && $rose(fst_clk)) begin
+            `ASSUME ($changed(slw_clk));
+         end else begin
+            `ASSUME ($stable(slw_clk));
+         end
+
+         if ($rose(slw_clk)) begin
+            assert (ctr == 0);
+         end
+      end
+
+      if (!past_valid) begin
+         `ASSUME (rst_n == 1'b0);
+      end else begin
+         `ASSUME (rst_n == 1'b1);
+      end
+
+      if (past_valid) begin
+         assert (out_ctr == ctr);
+      end
+   end
+
+   always @(posedge fst_clk) begin
+      if (rst_n) begin
+         out_ctr <= out_ctr + 1'b1;
+      end
+   end
+
+`endif
+
 `ifdef COCOTB_SIM
-   `ifdef PLL_SYNC_CTR
+ `ifdef PLL_SYNC_CTR
    initial begin
       $dumpfile ("cocotb/build/pll_sync_ctr.vcd");
       $dumpvars (0, pll_sync_ctr);
       #1;
    end
-   `endif
+ `endif
 `endif
 
 endmodule
