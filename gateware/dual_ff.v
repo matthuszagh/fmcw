fa8c5ce15903a3667774bda668a4478b43cb18aa
Matt Huszagh <huszaghmatt@gmail.com>
2020-02-23 05:42:08 -0800

    add dual_ff formal verification

--- gateware/dual_ff.v
+++ gateware/dual_ff.v
@@ -4,6 +4,8 @@
 `default_nettype none
 `timescale 1ns/1ps
 
+// Dual-edge triggered flip flop.
+
 module dual_ff #(
    parameter DATA_WIDTH = 1
 ) (
@@ -17,19 +19,41 @@ module dual_ff #(
 
    always @(posedge clk) begin
       if (!rst_n)
-        p <= 1'b0;
+        p <= {DATA_WIDTH{1'b0}};
       else
         p <= dp ^ n;
    end
 
    always @(negedge clk) begin
       if (!rst_n)
-        n <= 1'b0;
+        n <= {DATA_WIDTH{1'b0}};
       else
         n <= dn ^ p;
    end
 
    assign q = p ^ n;
+
+`ifdef FORMAL
+ `ifdef DUAL_FF
+  `define ASSUME assume
+ `else
+  `define ASSUME assert
+ `endif
+
+   (* gclk *) reg gclk;
+   reg     past_valid = 0;
+   always @(posedge gclk) begin
+      past_valid <= 1;
+      if ($past(rst_n) && past_valid) begin
+         if ($rose(clk)) begin
+            assert (q == $past(dp));
+         end else if ($fell(clk)) begin
+            assert (q == $past(dn));
+         end
+      end
+   end
+
+`endif
 
 endmodule
 `endif
