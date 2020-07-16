6d5bcaf6a837a10263e0bab612fd169021b6b2d8
Matt Huszagh <huszaghmatt@gmail.com>
2020-07-16 11:49:21 -0700

    vivado synth: only need to read top module

--- beta/gateware/verilog/scripts/vivado/synth.tcl
+++ beta/gateware/verilog/scripts/vivado/synth.tcl
@@ -1,11 +1,6 @@
 set src_dir src
 create_project top -in_memory -part xc7a15tftg256-1
-read_verilog -sv $src_dir/top.v \
-    $src_dir/async_fifo.v \
-    $src_dir/ram.v \
-    $src_dir/bin2gray.v \
-    $src_dir/ff_sync.v \
-    $src_dir/gray_ctr.v
+read_verilog -sv $src_dir/top.v
 read_xdc $src_dir/pinmap.xdc
 synth_design -name top \
     -top top \
