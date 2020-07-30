53314b2ed1be411fe4eb18012c6c7a08ad7004a8
Matt Huszagh <huszaghmatt@gmail.com>
2020-07-30 15:46:51 -0700

    shell.nix: use upstream openems

--- shell.nix
+++ shell.nix
@@ -3,6 +3,11 @@
   url = "https://github.com/nixos/nixpkgs/archive/44fd570d7344fb31d7dd92a42d6e1ef872b5f76b.tar.gz";
   sha256 = "0qxajv68s08m0fsyf4q6dscdn5c4j98hnhz4cls3hhiqvzz86cd1";
 }) {})
+, openems-pkgs ? (import (builtins.fetchTarball {
+  name = "matthuszagh-unstable-2020-04-06";
+  url = "https://github.com/matthuszagh/nixpkgs/archive/ad725f423cd187034e70ee639ae9eea751112c58.tar.gz";
+  sha256 = "0pdyzv4yzb8hscrsqmf3qshzsry0gx5mzc98gkbgab13yvhj35qp";
+}) {})
 }:
 
 let
@@ -24,6 +29,7 @@ let
     ]) ++ (with custompkgs; [
       # skidl
       pyems
+    ]) ++ (with openems-pkgs.python3Packages; [
       python-openems
       python-csxcad
     ]);
@@ -33,6 +39,7 @@ in
 pkgs.mkShell {
   buildInputs = with pkgs; [
     pythonEnv
+    custompkgs.pyems
     pkg-config
 
     # fpga
@@ -67,10 +74,11 @@ pkgs.mkShell {
     # curaengine
   ] ++ (with custompkgs; [
     ebase
+    vivado-2017-2
+  ]) ++ (with openems-pkgs; [
     openems
     appcsxcad
     hyp2mat
-    vivado-2017-2
   ]);
 
   KICAD_SYMBOL_DIR="/home/matt/src/kicad-symbols";
